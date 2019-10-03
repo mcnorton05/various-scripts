e3c6a6dd4784fe8f5196546c03d416804999470b
Michael Norton <mcnorton05@gmail.com>
2019-10-02 22:42:31 -0400

    new script

---
+++ hash-checker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#This script hashes a file and compared to a known hash value to see if they are the same.
+
+#get the file
+echo "Please enter the file path"
+read -r filepath
+
+#get the known hash value
+echo "Please enter the known hash value"
+read -r knownhash
+
+#hash the file
+filehash=$(shasum -a 256 "$filepath")
+
+echo "$filehash"
+
+#check to see if the hash values match
+if [ "$filehash" = "$knownhash" ]; then
+	echo "The integrity of this file is confirmed."
+else
+	echo "DANGER! The integrity of this file cannot be assured."
+fi

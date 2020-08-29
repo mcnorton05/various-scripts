49d8dcb7e591c29e1d898cd53ba26bc2c57d02e0
mcnorton05 <mcnorton05@gmail.com>
2020-08-29 19:36:26 -0400

    minor updates

--- hash-checker.sh
+++ hash-checker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#This script hashes a file and compared to a known hash value to see if they are the same.
+#This script hashes a file and compares to a known hash value to see if they are the same.
 
 #get the file
 echo "Please enter the file path"
 read -r filepath
 
 #get the known hash value
-echo "Please enter the known hash value"
+echo "Please enter the known SHA256 hash value"
 read -r knownhash
 
 #hash the file
